2b8cc9fa1e89df1d6c22556f385f87e28ea9aed4
Dmitri Nesterenko <dmitri@sentimentalistmag.com>
2015-05-03 01:20:08 -0400

    Refactor file and ID management to separate concerns

--- Phony/AppDelegate.swift
+++ Phony/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         //TODO: Move the code that creates the necessary internal folders if they do not exist here
+        
+        FileManager.createIfDoesNotExist(FileManager.recordingUrl())
+        
         return true
     }
 
